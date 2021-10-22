fc08161052204652094cc08d1b950fe9ae04f301
Michael Walker <walkerrepair255@yahoo.com>
2021-10-22 08:57:08 -0400

    Bot Battle

---
+++ random_number_battle/random_number_battle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace random_number_battle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
